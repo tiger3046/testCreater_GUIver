875dab3e02cdaf1ee2e1c5da6b3d1aea09d93654
tiger3046 <dawo73712@gmail.com>
2023-01-11 12:59:12 +0900

    There are three point of change. First,every file ,.html,.css,.js are in there category file. Second,design change for good to use,you use both  smartphone and laptopcomputer. Third,use right bocab from invender to inventor.

--- js/saeAns.js
+++ js/saeAns.js
@@ -1,57 +1,31 @@
-wordList = ['slow down','destination','reach','even though','too A to B','A enough to B','invender','gradually','nobody'
+wordListEn = ['slow down','destination','reach','even though','too A to B','A enough to B','inventor','gradually','nobody'
 ,'ridiculous','imagination','make fun of A','modern','in order to A','successful','find out','as soon as A'
 ,'unexpected','hesitate','separate','anymore','marry','still'];
 
-//重複のない乱数作成
-var randoms = [];
-var min = 1, max = wordList.length-3; 
-for(i = min; i <= max; i++){
-  while(true){
-    var tmp = intRandom(min, max);
-    if(!randoms.includes(tmp)){
-      randoms.push(tmp);
-      break;
-    }
-  }
-} 
-function intRandom(min, max){
-  return Math.floor( Math.random() * (max - min + 1)) + min;
-}
-console.log(randoms);
+wordListJa = ['ゆっくりになる','目的地','到達する','たとえ〜でさえ','AすぎてBできない','Bするのに十分Aである','発明家','しだいに','だれも,,,ない'
+,'滑稽な','想像力','Aをからかう','現代の','Aするために','成功した','分かる','Aするとすぐ'
+,'思いがけない','ためらう','分ける','もう','結婚する','まだ','AかBかどちらか'];
 
-//テスト作成
+
 let create_yukiEnbutton = document.getElementById('yukiEn_create');
 create_yukiEnbutton.addEventListener('click',function(e){
     alert('test creating start.');
-    for(let i=0;i<11;i++){
+    for(let i=0;i<wordListEn.length;i++){
         let tbl = document.getElementById("testTable");
         let row = document.createElement("tr");
         let cellText = '';
         for (let j=0; j<2; j++){
             let cell = document.createElement('td');
             if(j===0){
-                cellText  = document.createTextNode(wordList[randoms[i]]);
+                cellText  = document.createTextNode(wordListEn[i]);
             }else{
-                 cellText = document.createTextNode('　　　　　　　　　　　　　　　　');
+                 cellText = document.createTextNode(wordListJa[i]);
             }
             cell.appendChild(cellText);
             row.appendChild(cell);
         }
                 tbl.appendChild(row);
     }
-},false);
-
-//テスト削除機能
-let removeButton = document.getElementById('removeButton');
-removeButton.addEventListener('click', function(e){
-  alert('test remove!');
-    //ul要素を取得
-    let languageList = document.getElementById('testTable');
-    //リストの末尾から要素を削除
-    for(let i=0;i<11;i++){
-      languageList.removeChild(languageList.lastElementChild);
-    }
-    
 },false);
 
 //印刷機能
@@ -62,19 +36,16 @@ print_yukiEnbutton.addEventListener('click',function(e){
   let hypertext = document.getElementById('h');
   let paragraph2 = document.getElementById('p2');
   let create_button = document.getElementById('yukiEn_create');
-  let remove_button = document.getElementById('removeButton');
   let print_button = document.getElementById('yukiEn_print');
 
   paragraph1.style.visibility = "hidden";
   paragraph2.style.visibility = "hidden";
   hypertext.style.visibility = "hidden";
   create_button.style.visibility = "hidden";
-  remove_button.style.visibility = "hidden";
   print_button.style.visibility = "hidden";
 
   window.print();
 
-  remove_button.style.visibility = "visible";
   paragraph1.style.visibility = "visible";
   paragraph2.style.visibility = "visible";
   hypertext.style.visibility = "visible";
